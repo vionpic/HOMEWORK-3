65fb6fecd47245d94cea118ba6f94bd9ea393f44
vionpic <vionpic@gmail.com>
2019-09-22 23:39:05 +0700

    initial commit ab

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var x = document.getElementsByTagName("input")[0].value;
+    document.getElementsByTagName("input")[0].style.backgroundColor = "#FF4136";
+  }
+myFunction();
