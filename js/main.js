4c30c4ce0c88475a212de550149125b188761606
vionpic <vionpic@gmail.com>
2019-09-23 10:09:54 +0700

    fished

--- js/main.js
+++ js/main.js
@@ -73,5 +73,95 @@ document.getElementsByTagName("input")[1].onclick=function(){
 }
 
 function scroll() {
-    document.querySelector("div > div + div").scrollTop
+   var x= document.documentElement.scrollTop;
+   document.querySelector("div > div").style.top=+x+"px";
+}
+window.onscroll =function(){
+    scroll();
+}
+
+var clicks=0;
+function image(){
+    ++clicks;
+    if(clicks==5) clicks=0;
+    switch (clicks) {
+        case 1:
+            document.getElementsByTagName("img")[0].src="images/a.jpg";
+            break;
+        case 2 :
+            document.getElementsByTagName("img")[0].src="images/b.png";
+            break;
+        case 3 :
+            document.getElementsByTagName("img")[0].src="images/c.jpg";
+            break;
+        case 4 :
+            document.getElementsByTagName("img")[0].src="images/d.png";
+            break;
+        case 5 :
+            document.getElementsByTagName("img")[0].src="images/e.jpeg";
+            break;
+        default:
+            break;
+    }
+}
+function image1(){
+    --clicks;
+    if(clicks<0) clicks=5;
+    switch (clicks) {
+        case 1:
+            document.getElementsByTagName("img")[0].src="images/a.jpg";
+            break;
+        case 2 :
+            document.getElementsByTagName("img")[0].src="images/b.png";
+            break;
+        case 3 :
+            document.getElementsByTagName("img")[0].src="images/c.jpg";
+            break;
+        case 4 :
+            document.getElementsByTagName("img")[0].src="images/d.png";
+            break;
+        case 5 :
+            document.getElementsByTagName("img")[0].src="images/e.jpeg";
+            break;
+        default:
+            break;
+    }
+}
+document.getElementsByTagName("button")[1].onclick=function(){
+    image();
+}
+document.getElementsByTagName("button")[0].onclick=function(){
+    image1();
+}
+
+
+function avatar() {
+    var min=0; 
+	var max=5; 
+    var random = Math.floor(Math.random() * (+max - +min)) + +min;
+    switch (random) {
+        case 1:
+            document.getElementsByTagName("img")[0].src="images/a.jpg";
+            break;
+        case 2 :
+            document.getElementsByTagName("img")[0].src="images/b.png";
+            break;
+        case 3 :
+            document.getElementsByTagName("img")[0].src="images/c.jpg";
+            break;
+        case 4 :
+            document.getElementsByTagName("img")[0].src="images/d.png";
+            break;
+        case 5 :
+            document.getElementsByTagName("img")[0].src="images/e.jpeg";
+            break;
+        default:
+            break;
+    }
+}
+var counter=0;
+document.getElementsByTagName("img")[0].onclick=function(){
+    ++counter;
+    if(counter==1)
+    avatar();
 }
